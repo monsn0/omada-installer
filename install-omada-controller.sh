426b44c2c23e03b7f157c2acce18111e17cd1230
monsn0 <monsn0@users.noreply.github.com>
2021-07-31 23:16:12 -0400

    added support for Ubuntu 16.04

--- install-omada-controller.sh
+++ install-omada-controller.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 #title           :install-omada-controller.sh
-#description     :Omada Controller Installer for Ubuntu 18.04
+#description     :Omada Controller Installer for Ubuntu
 #author          :monsn0
 #date            :2021-07-29
 
 # URL of latest available version of the Omada Controller package
 OmadaPackageUrl=https://static.tp-link.com/software/2021/202107/20210701/Omada_SDN_Controller_v4.4.3_linux_x64.deb
 
+OS=$(hostnamectl | grep "Operating System")
+echo $OS
+
+if [[ $OS =  *"Ubuntu 16.04"* ]]; then
+    OsVer=xenial
+elif [[ $OS =  *"Ubuntu 18.04"* ]]; then
+    OsVer=bionic
+else
+    echo "$(tput setaf 1)ERROR: Script only supports Ubuntu 16.04 or 18.04!"
+    exit
+fi
+
 # Import the MongoDB 3.6 public key and add repo
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 58712A2291FA4AD5
-echo "deb http://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/3.6 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.6.list
-apt update
+echo "deb http://repo.mongodb.org/apt/ubuntu $OsVer/mongodb-org/3.6 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.6.list
 
 # Install/update dependencies
+apt update
 apt install openjdk-8-jre-headless -y
 apt install mongodb-org -y
 apt install jsvc -y
