0a2d44e8976d824402176426ff1ad6c3e976a820
monsn0 <monsn0@users.noreply.github.com>
2021-07-31 23:05:24 -0400

    added curl to ensure installed and up to date

--- install-omada-controller.sh
+++ install-omada-controller.sh
@@ -4,7 +4,7 @@
 #author          :monsn0
 #date            :2021-07-29
 
-# Update this variable with the URL to latest available version
+# URL of latest available version of the Omada Controller package
 OmadaPackageUrl=https://static.tp-link.com/software/2021/202107/20210701/Omada_SDN_Controller_v4.4.3_linux_x64.deb
 
 # Import the MongoDB 3.6 public key and add repo
@@ -12,10 +12,11 @@ apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 58712A2291FA4AD5
 echo "deb http://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/3.6 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.6.list
 apt update
 
-# Install the 3 dependencies not included in Ubuntu 18.04
+# Install/update dependencies
 apt install openjdk-8-jre-headless -y
 apt install mongodb-org -y
 apt install jsvc -y
+apt install curl -y
 
 # Download Omada Controller package and install
 wget $OmadaPackageUrl -P /tmp/
