05962db66f926fc58a1f185df3ff2a165daafbb8
monsn0 <monsn0@users.noreply.github.com>
2023-11-19 16:50:16 -0500

    Updated the adding of 3rd party key (closes #5)

--- install-omada-controller.sh
+++ install-omada-controller.sh
@@ -29,8 +29,8 @@ fi
 # Import the MongoDB 4.4 public key and add repo
 apt-get -qq update
 apt-get -qq install gnupg
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 656408E390CFB1F5
-echo "deb http://repo.mongodb.org/apt/ubuntu $OsVer/mongodb-org/4.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+curl -fsSL https://pgp.mongodb.com/server-4.4.asc | gpg -o /usr/share/keyrings/mongodb-server-4.4.gpg --dearmor
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-4.4.gpg ] https://repo.mongodb.org/apt/ubuntu $OsVer/mongodb-org/4.4 multiverse" > /etc/apt/sources.list.d/mongodb-org-4.4.list
 
 # Install/update dependencies
 apt-get -qq update
