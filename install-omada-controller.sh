c4b05d1dd149e414ba91e127095cfb23726e06e8
monsn0 <monsn0@users.noreply.github.com>
2023-11-19 23:46:42 -0500

    expand dependency installs and misc housekeeping

--- install-omada-controller.sh
+++ install-omada-controller.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 #title           :install-omada-controller.sh
-#description     :Omada Controller Installer for Ubuntu
+#description     :Installer for TP-Link Omada Software Controller
 #supported       :Ubuntu 16.04, Ubuntu 18.04, Ubuntu 20.04, Ubuntu 22.04
 #author          :monsn0
 #date            :2021-07-29
 #updated         :2023-11-19
 
 echo -e "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-echo -e "TP-Link Omada Software Controller - Installer \nhttps://github.com/monsn0/omada-installer"
+echo "TP-Link Omada Software Controller - Installer"
+echo "https://github.com/monsn0/omada-installer"
 echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
 
-echo "[+] Checking for supported OS"
+echo "[+] Verifying supported OS"
 OS=$(hostnamectl status | grep "Operating System")
 echo "[~] $OS"
 
@@ -32,23 +33,27 @@ else
     exit
 fi
 
-# Import the MongoDB 4.4 public key and add repo
-echo "[+] Importing the MongoDB 4.4 PGP key and creating repository"
+echo "[+] Installing script prerequisites"
 apt-get -qq update
-apt-get -qq install gnupg
+apt-get -qq install gnupg curl &> /dev/null
+
+echo "[+] Importing the MongoDB 4.4 PGP key and creating APT repository"
 curl -fsSL https://pgp.mongodb.com/server-4.4.asc | gpg -o /usr/share/keyrings/mongodb-server-4.4.gpg --dearmor
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-4.4.gpg ] https://repo.mongodb.org/apt/ubuntu $OsVer/mongodb-org/4.4 multiverse" > /etc/apt/sources.list.d/mongodb-org-4.4.list
 
-# Install/update dependencies
-echo "[+] Installing/updating OpenJDK 8, JSVC, MongoDB 4.4 and cURL"
+# Package dependencies
 apt-get -qq update
-apt-get -qq install openjdk-8-jre-headless mongodb-org jsvc curl &> /dev/null
+echo "[+] Installing OpenJDK 8 JRE (headless)"
+apt-get -qq install openjdk-8-jre-headless &> /dev/null
+echo "[+] Installing JSVC"
+apt-get -qq install jsvc &> /dev/null
+echo "[+] Installing MongoDB 4.4"
+apt-get -qq install mongodb-org &> /dev/null
 
-# Download Omada Controller package and install
-echo "[+] Downloading latest Omada SDN Controller package"
+echo "[+] Downloading the latest Omada Software Controller package"
 OmadaPackageUrl=$(curl -s https://www.tp-link.com/us/support/download/omada-software-controller/ | grep -oP '<a[^>]*href="\K[^"]*Linux_x64.deb[^"]*' | head -n 1)
 wget -qP /tmp/ $OmadaPackageUrl
-echo "[+] Installing Omada SDN Controller"
+echo "[+] Installing Omada Software Controller"
 dpkg -i /tmp/$(basename $OmadaPackageUrl) &> /dev/null
 
 hostIP=$(hostname -I | cut -f1 -d' ')
