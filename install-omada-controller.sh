74f04c947e22afec5851d128727422cd965d5e46
Kystre <nathan_doucet@hotmail.com>
2025-07-17 06:07:45 -0400

    Update install-omada-controller.sh
    added debian 12 detection and installation code sections

--- install-omada-controller.sh
+++ install-omada-controller.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #title           :install-omada-controller.sh
 #description     :Installer for TP-Link Omada Software Controller
-#supported       :Ubuntu 20.04, Ubuntu 22.04, Ubuntu 24.04
+#supported       :Ubuntu 20.04, Ubuntu 22.04, Ubuntu 24.04; Debian Linux 12
 #author          :monsn0
 #date            :2021-07-29
-#updated         :2025-03-31
+#updated         :2025-07-16
 
 echo -e "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo "TP-Link Omada Software Controller - Installer"
@@ -29,15 +29,25 @@ echo "[~] $OS"
 
 if [[ $OS = *"Ubuntu 20.04"* ]]; then
     OsVer=focal
+    OsType=ubuntu
 elif [[ $OS = *"Ubuntu 22.04"* ]]; then
     OsVer=jammy
+    OsType=ubuntu
 elif [[ $OS = *"Ubuntu 24.04"* ]]; then
     OsVer=noble
+    OsType=ubuntu
+elif [[ $OS = *"Debian GNU/Linux 12"* ]]; then
+    OsVer=bookworm
+    OsType=debian
 else
-    echo -e "\e[1;31m[!] Script currently only supports Ubuntu 20.04, 22.04 or 24.04! \e[0m"
+    echo -e "\e[1;31m[!] \n Script currently only supports the following: \n APT: Ubuntu 20.04, 22.04, 24.04; Debian 12 Bookworm\n[!] \e[0m"
     exit
 fi
 
+
+
+
+install_ubuntu() {
 echo "[+] Installing script prerequisites"
 apt-get -qq update
 apt-get -qq install gnupg curl &> /dev/null
@@ -62,6 +72,43 @@ apt-get -qq install jsvc &> /dev/null
 
 echo "[+] Installing Omada Software Controller $(echo $OmadaPackageBasename | tr "_" "\n" | sed -n '4p')"
 dpkg -i /tmp/$OmadaPackageBasename &> /dev/null
+}
+
+
+install_debian() {
+echo "[+] Installing script prerequisites"
+apt-get -qq update
+apt-get -qq install gnupg curl &> /dev/null
+
+echo "[+] Importing the MongoDB 8.0 PGP key and creating the APT repository"
+curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg --dearmor
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/debian $OsVer/mongodb-org/8.0 main" > /etc/apt/sources.list.d/mongodb-org-8.0.list
+apt-get -qq update
+
+echo "[+] Downloading the latest Omada Software Controller package"
+OmadaPackageUrl=$(curl -fsSL https://support.omadanetworks.com/us/product/omada-software-controller/?resourceType=download | grep -oPi '<a[^>]*href="\K[^"]*linux_x64_[0-9]*\.deb[^"]*' | head -n 1)
+OmadaPackageBasename=$(basename $OmadaPackageUrl)
+curl -sLo /tmp/$OmadaPackageBasename $OmadaPackageUrl
+
+# Package dependencies
+echo "[+] Installing MongoDB 8.0"
+apt-get -qq install mongodb-org &> /dev/null
+echo "[+] Installing OpenJDK 21 JRE (headless)"
+apt-get -qq install openjdk-21-jre-headless &> /dev/null
+echo "[+] Installing JSVC"
+apt-get -qq install jsvc &> /dev/null
+
+echo "[+] Installing Omada Software Controller $(echo $OmadaPackageBasename | tr "_" "\n" | sed -n '4p')"
+dpkg -i /tmp/$OmadaPackageBasename &> /dev/null
+}
+
+
+if [ "$OsType" = "ubuntu" ]; then
+    install_ubuntu
+else [ "$OsType" = "debian" ]; then
+    install_debian
+fi
+
 
 hostIP=$(hostname -I | cut -f1 -d' ')
 echo -e "\e[0;32m[~] Omada Software Controller has been successfully installed! :)\e[0m"
